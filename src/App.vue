b758ce4d8795269852714094fb27a052f5fe581e
gamerlife1996 <ericchan@xlcwnet.com>
2022-01-27 18:02:28 +0800

    project files

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
+      { title: 'About', icon: 'mdi-help-box', to: '/about' },
+    ],
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home pa-6">
+    <v-text-field
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    ></v-text-field>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+      :items-per-page=-1
+    >
+      <template v-slot:item.image="{item}">
+        <v-img :src="item.image" width="198px" height="40px" />
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import json from './data.json'
+var tabledata = []
+for (var i = 0; i < json.shops.length; i++)
+{
+  var shop = json.shops[i];
+  for (var j = 0; j < shop.goods.length; j++)
+  {
+    var good = shop.goods[j];
+    tabledata.push({
+      image: "1-1/"+good.index+".jpg",
+      name: good.name,
+      price: good.price,
+      shop: shop.title,
+      map: "1-1",
+      avail: good.available,
+      time: json.time,
+    });
+  }
+}
+  export default {
+    data() {
+      return {
+        name: 'Home',
+        search: '',
+        headers: [
+          { text: '截图', value: 'image' },
+          { text: '名字', value: 'name' },
+          { text: '价格', value: 'price' },
+          { text: '商店', value: 'shop' },
+          { text: '位置', value: 'map' },
+          { text: '有货', value: 'avail' },
+          { text: '时间', value: 'time' },
+        ],
+        desserts: tabledata,
+      }
+    }
+  }
+</script>
